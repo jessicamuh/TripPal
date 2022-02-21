fc6306c3aea1c06ff0d594c3ac218089eab09dc3
Gracieux Mwisa <gracieuxmwisa@Gracieuxs-MacBook-Pro.local>
2022-02-20 21:36:30 -0500

    created diffrent components

--- TripPal/src/app/app.module.ts
+++ TripPal/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './signup/signup.component';
+import { CredentialsComponent } from './signup/credentials/credentials.component';
+import { AboutmeComponent } from './signup/aboutme/aboutme.component';
+import { ChildrenComponent } from './signup/children/children.component';
+import { SkillsComponent } from './signup/skills/skills.component';
+import { AccountcreatedComponent } from './accountcreated/accountcreated.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    WelcomeComponent,
+    SignupComponent,
+    CredentialsComponent,
+    AboutmeComponent,
+    ChildrenComponent,
+    SkillsComponent,
+    AccountcreatedComponent
   ],
   imports: [
     BrowserModule
