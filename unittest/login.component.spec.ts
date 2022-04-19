5c2b4f15f503fb3b7987d256bf7ba08b406eac24
jessicamuh <49791345+jessicamuh@users.noreply.github.com>
2022-04-18 21:56:13 -0400

    Update login.component.spec.ts

--- unittest/login.component.spec.ts
+++ unittest/login.component.spec.ts
@@ -1,16 +1,26 @@
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick,} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+import {By} from '@angular/platform-browser';
 
-import { LoginComponent } from './login.component';
+const mockAuthService = jasmine.createSpyObj('AuthService', [
+  'loginWithEmailAndPassword',
+  'signUpWithEmailAndPassword',
+  'signOut',
+]);
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+    return await TestBed.configureTestingModule({
+      declarations: , const: [LoginComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -52,4 +62,34 @@ describe('Create Account button', () => {
     expect(component.create_account).toHaveBeenCalled();
   }));
 });
+});
+
+describe('Login method', ()  => {
+  it('should call the auth service login with email and password method', () => {
+    mockAuthService.loginWithEmailAndPassword.and.resolveTo({});
+
+    component.login();
+
+    expect(mockAuthService.loginWithEmailAndPassword).toHaveBeenCalled();
+  });
+
+  it('should set the value of the authError variable when an error is thrown from the auth service', fakeAsync (() => {
+    mockAuthService.loginWithEmailAndPassword.and.rejectWith(new Error('Error'));
+
+    component.login();
+    tick();
+
+    expect(mockAuthService.loginWithEmailAndPassword).toHaveBeenCalled();
+    expect(component.authError.toString()).toEqual('Error: Error');
+  }));
+
+  it('should pass the sign in form value to the auth service method', () => {
+    mockAuthService.loginWithEmailAndPassword.and.resolveTo({});
+    const loginFormValue = {email: 'foo', password: 'bar'};
+    component.loginForm.setValue(loginFormValue);
+
+    component.login();
+
+    expect(mockAuthService.loginWithEmailAndPassword).toHaveBeenCalledWith(loginFormValue);
+  });
 });
